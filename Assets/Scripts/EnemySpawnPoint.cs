3cd24d3d9de66b9ccd9246d18195365d4566105a
Cajova Houba <tkmushroomer@gmail.com>
2020-04-11 11:37:29 +0200

    Base of the script for one enemy spawn point.

---
+++ Assets/Scripts/EnemySpawnPoint.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnPoint : MonoBehaviour
+{
+    /// <summary>
+    /// Range used to detect whether player is nearby or not.
+    /// </summary>
+    public int spawnPointRange = 10;
+
+    public LayerMask playerLayer;
+
+    /// <summary>
+    /// Checks if the player is in range of this spawn point.
+    /// </summary>
+    /// <returns></returns>
+    public bool IsPlayerNear()
+    {
+        Collider2D[] playerColisions = Physics2D.OverlapCircleAll(transform.position, spawnPointRange, playerLayer);
+        return playerColisions.Length > 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    /// <summary>
+    /// Draw small circle around attack point - good for debugging.
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, spawnPointRange);
+    }
+}
