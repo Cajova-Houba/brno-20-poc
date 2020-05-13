e6bcbadd2fc30e014631893734a51152acb55bcc
Cajova Houba <tkmushroomer@gmail.com>
2020-05-13 21:44:28 +0200

    Small errors.

--- Assets/Powerups/PickedUpPowerup.cs
+++ Assets/Powerups/PickedUpPowerup.cs
@@ -14,16 +14,6 @@ namespace Assets.Powerups
     public class PickedUpPowerup : MonoBehaviour
     {
         /// <summary>
-        /// Name of the trigger for CigaPowerup animation.
-        /// </summary>
-        public const string CIGA_ANIM_TRIG = "ciga";
-
-        /// <summary>
-        /// Name of the trigger for SmazoPowerup animation.
-        /// </summary>
-        public const string SMAZO_ANIM_TRIG = "smazo";
-
-        /// <summary>
         /// Controller used to display animation of picked up items.
         /// </summary>
         public Animator animator;
@@ -31,19 +21,6 @@ namespace Assets.Powerups
         public void TriggerAnimation(string triggerName)
         {
             animator.SetTrigger(triggerName);
-        }
-
-        /// <summary>
-        /// Activates 'ciga' trigger.
-        /// </summary>
-        public void DisplayCiga()
-        {
-            animator.SetTrigger(CIGA_ANIM_TRIG);
-        }
-
-        public void DisplaySmazo()
-        {
-            animator.SetTrigger(SMAZO_ANIM_TRIG);
         }
 
         void Start()
